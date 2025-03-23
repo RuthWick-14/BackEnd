1157c5ae666859f184a3392c0d942b942f8a1ff9
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-03-24 00:02:23 +0530

    12

--- 12_Logic_building/backend/src/controller/user.controller.js
+++ 12_Logic_building/backend/src/controller/user.controller.js
@@ -1,9 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
-const registerUser = asyncHandler( async(req,res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+const registerUser = asyncHandler(async (req, res) => {
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refres token field from response
+    // check for user user creation 
+    // return response
+    
+    const {fullName, email, username, password} = req.bod
+    console.log("email: ", email);
+
+    if (fullName === "") {
+        throw new ApiError(400, "fullname is required.")
+    }
 })
 
 export { registerUser }
